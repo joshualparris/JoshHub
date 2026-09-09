6412f2e099174e0f0b86ff03a603631c2c1bc714
Joshua Luke Parris <130725599+joshualparris@users.noreply.github.com>
2026-09-09 21:46:02 +1000

    refactor: remove proven dead source modules

--- src/lib/react/useHydrated.ts
+++
@@ -1,9 +0,0 @@
-import { useSyncExternalStore } from "react";
-
-export default function useHydrated(): boolean {
-  return useSyncExternalStore(
-    () => () => {},
-    () => true,
-    () => false
-  );
-}
